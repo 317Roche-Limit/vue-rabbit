74b440c9a59eec97e8c262e3e7773d86c167135b
sleeping <m13829635831@163.com>
2025-05-08 14:49:46 +0800

    home-人气推荐业务实现

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -15,12 +15,5 @@ import HomePanel from './components/HomePanel.vue'
   <HomeNew />
   <HomeHot />
   <HomeProduct />
-  <!-- 测试面板组件 -->
-  <HomePanel title="新鲜好物" subTitle="新鲜好物 好多商品">
-    <div>我是新鲜好物</div>
-  </HomePanel>
 
-  <HomePanel title="人气推荐" subTitle="人气推荐 好多商品">
-    <div>我是人气推荐</div>
-  </HomePanel>
 </template>
