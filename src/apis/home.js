8fec97930f3bb39e4d272990860ed0d3875061f3
sleeping <m13829635831@163.com>
2025-05-08 14:42:33 +0800

    home-新鲜好物业务实现

--- src/apis/home.js
+++ src/apis/home.js
@@ -1,7 +1,15 @@
 import httpInstance from "@/utils/http";
 
+// 获取banner数据接口
 export function getBannerAPI(){
     return httpInstance({
         url:'/home/banner'
+    })
+}
+
+// 获取新鲜好物数据接口
+export function findNewAPI(){
+    return httpInstance({
+        url:'/home/new'
     })
 }
