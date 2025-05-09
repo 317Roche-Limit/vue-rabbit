10daf8d1e0a0a5a01f669767247610b8fc36ff9d
sleeping <m13829635831@163.com>
2025-05-09 17:13:16 +0800

    home-轮播图实现

--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -1,8 +1,10 @@
 <script setup>
+import {getBannerAPI} from '@/apis/home'
 import {getTopCategoryAPI} from '@/apis/category'
 import { onMounted,ref } from 'vue';
 import {useRoute} from 'vue-router'
 
+// 获取数据
 const categoryDate = ref({})
 const route = useRoute()
 const getCategory = async()=>{
@@ -11,7 +13,20 @@ const getCategory = async()=>{
 }
 console.log(route);
 onMounted(()=>getCategory())
+
+// 获取banner
+const bannerList = ref([])
+const getBanner = async ()=>{
+    const res = await getBannerAPI({
+        distributionSite:'2'
+    })
+    bannerList.value = res.result
+}
+onMounted(()=>{
+    getBanner()
+})
 </script>
+
 
 <template>
   <div class="top-category">
@@ -23,6 +38,14 @@ onMounted(()=>getCategory())
           <el-breadcrumb-item>{{categoryDate.name}}</el-breadcrumb-item>
         </el-breadcrumb>
       </div>
+      <!-- 轮播图 -->
+        <div class="home-banner">
+            <el-carousel height="500px">
+            <el-carousel-item v-for="item in bannerList" :key="item.id">
+                <img :src="item.imgUrl" alt="">
+            </el-carousel-item>
+            </el-carousel>
+        </div>
     </div>
   </div>
 </template>
@@ -104,6 +127,17 @@ onMounted(()=>getCategory())
 
   .bread-container {
     padding: 25px 0;
+  }
+}
+
+.home-banner {
+  width: 1240px;
+  height: 500px;
+  margin:0,auto;
+
+  img {
+    width: 100%;
+    height: 500px;
   }
 }
 </style>
