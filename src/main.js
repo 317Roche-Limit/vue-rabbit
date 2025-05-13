fe971c34d722152c4d82547deac14b8e43a4038b
sleeping <m13829635831@163.com>
2025-05-13 23:28:00 +0800

    详情页-全局组件插件化

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import {componentPlugin} from '@/components/index'
 
 // 引入初始化样式文件
 import '@/styles/common.scss'
@@ -21,6 +22,7 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 app.use(lazyPlugin)
+app.use(componentPlugin)
 
 app.mount('#app')
 
